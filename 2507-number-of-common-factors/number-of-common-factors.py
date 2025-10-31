b1b9604cd088e6ba1a96dcce4c6a1a1ed8f4e114
sudhirrawol <114053375+Sudhirrawol@users.noreply.github.com>
2025-10-31 21:16:34 +0530

    Time: 3 ms (27.54%) | Memory: 12.4 MB (82.84%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        min_value = a if a < b else b
+
+        count =0
+
+        for i in range(1 , min_value+1):
+         if a % i ==0 and b %i ==0:
+          count +=1
+        return count  
