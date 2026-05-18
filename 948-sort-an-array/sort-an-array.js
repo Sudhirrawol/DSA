721e439dbe989002d4bcb6ec8c7cd74e2a9e3162
sudhirrawol <114053375+Sudhirrawol@users.noreply.github.com>
2026-05-19 00:33:39 +0530

    Time: 65 ms (80.09%) | Memory: 74.3 MB (90.79%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -1,8 +1,48 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var sortArray = function(nums) {
-    const array =nums.sort((a,b)=>a-b)
-    return array
+
+    function mergeSort(i, j, arr) {
+
+        if (i >= j) return;
+
+        let mid = Math.floor((i + j) / 2);
+
+        mergeSort(i, mid, arr);
+        mergeSort(mid + 1, j, arr);
+
+        merge(i, mid, j, arr);
+    }
+
+    function merge(i, mid, j, arr) {
+
+        let left = i;
+        let right = mid + 1;
+        let temp = [];
+
+        while (left <= mid && right <= j) {
+            if (arr[left] <= arr[right]) {
+                temp.push(arr[left]);
+                left++;
+            } else {
+                temp.push(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push(arr[left]);
+            left++;
+        }
+
+        while (right <= j) {
+            temp.push(arr[right]);
+            right++;
+        }
+
+        for (let k = 0; k < temp.length; k++) {
+            arr[i + k] = temp[k];
+        }
+    }
+
+    mergeSort(0, nums.length - 1, nums);
+    return nums;
 };
