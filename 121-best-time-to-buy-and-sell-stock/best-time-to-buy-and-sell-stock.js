3ff735dbba06f311306ea8231a56a6d1ab926feb
sudhirrawol <114053375+Sudhirrawol@users.noreply.github.com>
2026-05-22 22:55:10 +0530

    Time: 2 ms (80.79%) | Memory: 64.3 MB (94.29%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+        let buy = 0;
+    let sell = 1;
+    let maxProfit = 0;
+    while (sell < prices.length) {
+        if (prices[buy] > prices[sell]) {
+            buy = sell
+
+        } else {
+            let profit = prices[sell] - prices[buy]
+            if (profit > maxProfit) {
+                maxProfit = profit
+            }
+        }
+
+
+        sell++
+
+    }
+    
+    return maxProfit;
+};
