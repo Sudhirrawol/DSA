25bd1b785c9101eecf847580e5c49adc62e0ec99
sudhirrawol <114053375+Sudhirrawol@users.noreply.github.com>
2026-05-24 12:33:34 +0530

    Time: 1 ms (69.37%) | Memory: 57.3 MB (38.27%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(arr) {
+        let sum = 0;
+    let n = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    let total = n * (n + 1) / 2;
+    return total - sum;
+};
